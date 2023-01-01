7b78cee4ddc3ddccf5e365bfb2ac3866e8530279
Petr Ilin <hevav@hevav.dev>
2023-01-01 22:52:45 +0300

    Happy new year

--- src/main/java/net/elytrium/fastprepare/handler/PreparedPacketEncoder.java
+++ src/main/java/net/elytrium/fastprepare/handler/PreparedPacketEncoder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 - 2022 Elytrium
+ * Copyright (C) 2021 - 2023 Elytrium
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -48,7 +48,7 @@ public class PreparedPacketEncoder extends ChannelOutboundHandlerAdapter {
   }
 
   @Override
-  public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+  public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) {
     if (msg instanceof DummyPacket) {
       return;
     }
